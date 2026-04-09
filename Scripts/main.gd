bc4e4d3a22d6aac10872f3271eacbf152a72fe0d
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2026-04-09 10:56:18 +0200

    Display all level stats on UI (score, timer, fails and success)

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -14,6 +14,11 @@ var level_running := false
 @onready var order_manager = $OrderManager
 @onready var camera = $Camera3D
 
+@onready var score_label = $CanvasLayer/ScoreLabel
+@onready var timer_label = $CanvasLayer/TimerLabel
+@onready var success_label = $CanvasLayer/SuccessLabel
+@onready var fail_label = $CanvasLayer/FailLabel
+
 func _ready():
 	var items = get_tree().get_nodes_in_group("item")
 	for item in items:
@@ -30,12 +35,12 @@ func _ready():
 func start_level():
 	var level_data = level_manager.get_current_level()
 
-	fail_count = 0
 	success_count = 0
-	max_failures = level_data["max_failures"]
+	fail_count = 0
 	score = 0
 	level_time_left = level_data["time_limit"]
 	target_success = level_data["target_success"]
+	max_failures = level_data["max_failures"]
 	level_running = true
 
 	setup_level()
@@ -43,6 +48,8 @@ func start_level():
 	order_manager.set_level_orders(level_data["allowed_orders"])
 	order_manager.clear_orders()
 	order_manager.start_orders(2)
+
+	update_level_ui()
 
 	print("START LEVEL :", level_data["name"])
 	print("Target success :", target_success)
@@ -62,6 +69,19 @@ func setup_level():
 			body.process_mode = Node.PROCESS_MODE_INHERIT if enabled else Node.PROCESS_MODE_DISABLED
 			body.set_collision_layer_value(2, enabled)
 
+func update_level_ui():
+	if score_label:
+		score_label.text = "Score : " + str(score)
+
+	if timer_label:
+		timer_label.text = "Temps : " + str(int(ceil(level_time_left))) + "s"
+
+	if success_label:
+		success_label.text = "Succès : " + str(success_count) + " / " + str(target_success)
+
+	if fail_label:
+		fail_label.text = "Erreurs : " + str(fail_count) + " / " + str(max_failures)
+
 func _input(event):
 	if event is InputEventMouseButton and not event.pressed:
 		if held_item != null:
@@ -74,6 +94,7 @@ func _process(delta):
 		if level_time_left <= 0:
 			level_time_left = 0
 			end_level(false)
+		update_level_ui()
 
 	if held_item != null:
 		var mouse_pos = get_viewport().get_mouse_position()
@@ -136,6 +157,8 @@ func register_success():
 	success_count += 1
 	score += 10
 
+	update_level_ui()
+
 	print("SUCCESS COUNT :", success_count, "/", target_success)
 	print("SCORE :", score)
 
@@ -145,6 +168,8 @@ func register_success():
 func register_fail():
 	fail_count += 1
 	score -= 5
+
+	update_level_ui()
 
 	print("FAIL COUNT :", fail_count, "/", max_failures)
 	print("SCORE :", score)
@@ -160,6 +185,7 @@ func end_level(win: bool):
 		return
 
 	level_running = false
+	update_level_ui()
 
 	if win:
 		print("LEVEL COMPLETE")
